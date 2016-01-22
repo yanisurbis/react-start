c6cfaf4a5f8a1195ac8b2b3edb2af97298a5521d
Yanis <yanis.urbis@gmail.com>
2016-01-22 23:47:24 +0300

    Yet another commit

--- 7-firebase/src/app.jsx
+++ 7-firebase/src/app.jsx
@@ -1,14 +1,22 @@
 var React = require('react');
-var Dropdown = require('./dropdown');
+var ThumbnailList = require('./thumbnail-list');
 
 var options = {
-  title: 'Choose a dessert', // What should show up on the button to open/close the dropdown
-  items: [ // List of items to show in the dropdown
-    'Apple Pie',
-    'Peach Cobbler',
-    'Coconut Cream Pie'
-  ]
+  thumbnailData:  [{
+    title: 'Show Courses',
+    number: 120,
+    header: 'Learn React',
+    description: 'React is a fantastic new front end library for rendering web pages. React is a fantastic new front end library for rendering web pages.',
+    imageUrl: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png'
+  },{
+    title: 'Show Courses',
+    number: 25,
+    header: 'Learn Gulp',
+    description: 'Gulp will speed up your development workflow.  Gulp will speed up your development workflow.  Gulp will speed up your development workflow.',
+    imageUrl: 'http://brunch.io/images/others/gulp.png'
+  }]
 };
 
-var element = React.createElement(Dropdown, options);
+
+var element = React.createElement(ThumbnailList, options);
 React.render(element, document.querySelector('.container'));
